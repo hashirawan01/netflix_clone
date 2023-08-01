a374ed86860bdca7f790a168bc6e0b5b44711704
Hashir Ali Awan <malikhashiraliawan@gmail.com>
2023-08-01 15:14:54 +0500

    fnal commit

--- lib/Screens/SearchScreen.dart
+++ lib/Screens/SearchScreen.dart
@@ -20,94 +20,96 @@ class _SearchScreenState extends State<SearchScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.black,
-      appBar: AppBar(
+    return SingleChildScrollView(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.black,
-        leading: Icon(
-          Icons.search,
-          color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          title: TextField(
+            decoration: InputDecoration(
+                hintText: "Search For a movie show,",
+                hintStyle: TextStyle(color: Colors.white),
+                suffixIcon: IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.mic,
+                      color: Colors.grey,
+                    ))),
+          ),
         ),
-        title: TextField(
-          decoration: InputDecoration(
-              hintText: "Search For a movie show,",
-              hintStyle: TextStyle(color: Colors.white),
-              suffixIcon: IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.mic,
-                    color: Colors.grey,
-                  ))),
-        ),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Text(
-              "POPULAR MOVIES",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top:16.0),
-              child: SizedBox(
-                height: 590,
-                child:  FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                  future: getData(),
-                  // Specify the type of data expected in the future builder
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return Lottie.asset("assets/lottie/animation.json");
-                      print("hellow bBY");
-                    } else {
-                      print("hellow bBY TWO");
-                      final data = snapshot.data?.docs ??
-                          []; // Use null-aware operator and null-coalescing operator to handle null case
-                      return ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (_, index) {
-                          final docData = data[index].data();
-                          return Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Container(
-                              decoration: BoxDecoration( color: Colors.blueGrey),
-                              child: GestureDetector(
-                                onTap: null,
-                                child: SizedBox(
-                                  child: Row(
-                                    children: [
-                                      SizedBox(
-                                        height: 100,
-                                        width: 200,
-                                        child: Image.network(data[index]
-                                        ['image'],fit: BoxFit.fitWidth,), // Use null-aware operator
-                                      ),
-                                      Container(
-                                        width: 100, child: Padding(
-                                          padding: const EdgeInsets.only(left:8.0),
-                                          child: Text( docData['name']
-                                      ,style: TextStyle(color: Colors.white,fontSize: 18.0),),
+        body: Container(
+          child: Column(
+            children: [
+              Text(
+                "POPULAR MOVIES",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:16.0),
+                child: SizedBox(
+                  height: 590,
+                  child:  FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                    future: getData(),
+                    // Specify the type of data expected in the future builder
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return Lottie.asset("assets/lottie/animation.json");
+                        print("hellow bBY");
+                      } else {
+                        print("hellow bBY TWO");
+                        final data = snapshot.data?.docs ??
+                            []; // Use null-aware operator and null-coalescing operator to handle null case
+                        return ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (_, index) {
+                            final docData = data[index].data();
+                            return Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Container(
+                                decoration: BoxDecoration( color: Colors.blueGrey),
+                                child: GestureDetector(
+                                  onTap: null,
+                                  child: SizedBox(
+                                    child: Row(
+                                      children: [
+                                        SizedBox(
+                                          height: 100,
+                                          width: 200,
+                                          child: Image.network(data[index]
+                                          ['image'],fit: BoxFit.fitWidth,), // Use null-aware operator
                                         ),
-                                      ),
-                                      Icon(Icons.play_arrow,color: Colors.grey,)
-                                    ],
+                                        Container(
+                                          width: 100, child: Padding(
+                                            padding: const EdgeInsets.only(left:8.0),
+                                            child: Text( docData['name']
+                                        ,style: TextStyle(color: Colors.white,fontSize: 18.0),),
+                                          ),
+                                        ),
+                                        Icon(Icons.play_arrow,color: Colors.grey,)
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  },
+                            );
+                          },
+                        );
+                      }
+                    },
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
